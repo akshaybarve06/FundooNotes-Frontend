40bb565086b702ffaffc0e1d1a11faf20d57355f
akshaybarve06 <akshaybarve06@gmail.com>
2020-05-27 09:48:10 +0530

    Added Login Page To Login User

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Register from "../views/Register.vue"
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   },
   {
     path: "/register",
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="container">
-    <h1>Register Here</h1>
+    <h1>Login Form</h1>
     <br />
     <div class="row">
       <div class="col-md-3"></div>
       <div class="col-md-6">
         <form>
           <div class="form-group">
-            <input type="text"
-            class="form-control"
-            id="fullname"
-            placeholder="Enter Full Name" />
-          </div>
-          <br />
-          <div class="form-group">
-            <input type="text"
-            class="form-control"
-            id="mobile" 
-            placeholder="Enter Mobile Number" />
-          </div>
-          <br />
-          <div class="form-group">
             <input
               type="email"
               class="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
-              placeholder="Enter Email Id"
+              placeholder="Enter email"
             />
             <small id="emailHelp" class="form-text text-muted"></small>
           </div>
@@ -36,14 +22,14 @@
               type="password"
               class="form-control"
               id="exampleInputPassword1"
-              placeholder="Enter Password"
+              placeholder="Password"
             />
           </div>
           <br />
-          <button type="submit" class="btn btn-primary">Register</button>
+          <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
-      <div class="col-md-3"></div>
+      <div class="col-md-4"></div>
     </div>
   </div>
 </template>
@@ -52,5 +38,6 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
+
 </style>
